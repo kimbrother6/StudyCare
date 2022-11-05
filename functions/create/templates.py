355ec4587adaa4857be1d2991cc25068bd501895
cubest_june <kim.hyung.june.08@gmail.com>
2022-11-05 17:48:18 +0900

    버그 수정
    functions/create/templates.py의 버그 수정
    date.year -> date['year'] 등등

--- functions/create/templates.py
+++ functions/create/templates.py
@@ -3,9 +3,9 @@ from functions.create import date
 def get_todo_template():
 	"새로운 TODO템플릿을 만들고, 그것과 파일이름 반환"
 
-	template = f"{date.year}년 {date.week}주차({date.monday}~{date.sunday}) TODO\n[] - "
+	template = f"{date['year']}년 {date['week']}주차({date['monday']}~{date['sunday']}) TODO\n[] - "
 
-	template_name= f"{date.year}_{date.week}_TODO"
+	template_name= f"{date['year']}_{date['week']}_TODO"
 
 
 	return template, template_name 
