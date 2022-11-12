048d918a46632e7dd516ffdb9a625f62be5617f5
cubest_june <kim.hyung.june.08@gmail.com>
2022-11-12 18:48:54 +0900

    Schedule 출력 기능 추가
    DB_cmd.py :
    add get_this_week_schedules funtion
    add get_schedules function
    
    templates.py :
    Schedule 출력 기능 추가

--- DB_cmd.py
+++ DB_cmd.py
@@ -31,10 +31,39 @@ def DB_set_reference_day(user, reference_day):
 
 def get_reference_day(user):
 	mycursor.execute(f"SELECT reference_day FROM StudyCare.setting WHERE user='{user}'")
-	# 추가로 조건: WHERE id = 2, 정렬: 'ORDER BY name', 거꾸로 정렬: 'ORDER BY name DESC' 사용가능
 
 	reference_day = mycursor.fetchall()[0][0]
 	return reference_day
+
+def get_this_week_schedule(year, week):
+
+	schedules = get_schedule(year, week)
+
+	this_week_schedule = []
+
+	for schedule in schedules:
+		#이 일정이 이번주 일정인지 확인하기 위한 boolean들
+		cycle_state = schedule[5]
+		cycle_of_repetition = cycle_state % 10
+		is_repetition = cycle_state > 1
+		is_right_repetition = (week - schedule[1]) % cycle_of_repetition == 0
+		is_no_reptition = schedule[0] == schedule[2] and schedule[1] == schedule[3]
+		is_this_week_schedule = (is_repetition and is_right_repetition) or is_no_reptition
+		
+		if is_this_week_schedule:
+			this_week_schedule.append(schedule[4])
+
+	return this_week_schedule
+
+def get_schedules(year, week):
+
+	mycursor.execute(f"""
+	SELECT st_year, st_week, ed_year, ed_week, content, cycle FROM StudyCare.Schedule 
+	WHERE (st_year = {year} and st_week <= {week}) or (st_year < {year} and {year} < ed_year) or ({year} = ed_year and {week} <= ed_week)""")
+
+	schedules = mycursor.fetchall()
+
+	return schedules
 
 
 def create(data_bundle):
--- functions/create/templates.py
+++ functions/create/templates.py
@@ -1,12 +1,19 @@
 from functions.create import get_date
-
+from DB_cmd import get_this_week_schedule
 
 def get_todo_template(reference_day):
 	"새로운 TODO템플릿을 만들고, 그것과 파일이름 반환"
 	
 	date = get_date(reference_day)
+	schedules = get_this_week_schedule(date['year'], date['week'])
 
-	template = f"{date['year']}년 {date['week']}주차({date['monday']}~{date['sunday']}) TODO\n[] - "
+	template = f"{date['year']}년 {date['week']}주차({date['monday']}~{date['sunday']}) TODO\n"
+
+	if schedules:
+		for schedule in schedules:
+			template += f"[] - {schedule}\n"
+	else:
+		template += "[] - "
 
 	template_name= f"{date['year']}_{date['week']}_TODO"
 
