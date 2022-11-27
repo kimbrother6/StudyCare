7743d009393e5a2813017ccec8467d8fedb791b8
cubest_june <kim.hyung.june.08@gmail.com>
2022-11-27 23:09:07 +0900

    오류 수정

--- DB_cmd/reference_day.py
+++ DB_cmd/reference_day.py
@@ -3,7 +3,7 @@ from DB_cmd import mycursor, mydb
 def DB_set_reference_day(user: str, reference_day: int) -> None:
 	"user의 새로운 생성기준일 설정"
 
-	update = f"""UPDATE StudyCare.settin정
+	update = f"""UPDATE StudyCare.setting
 	SET reference_day={reference_day}
 	WHERE user='{user}'
 	"""
